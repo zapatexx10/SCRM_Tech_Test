7c8003e6fdcbcdcfd219455c1c08e9bb77acea29
Marc Zapata <mzapata@blinkay.com>
2026-02-25 23:51:17 +0100

    Add promotionsControllerTests (unit)

--- test/Application.Tests/GetAll/V1/PromotionsControllerITests.cs
+++ test/Application.Tests/GetAll/V1/PromotionsControllerITests.cs
@@ -1,54 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 
-namespace PromotionEngine.Application.GetAll.V1
+namespace PromotionEngine.Application.GetAll.V1;
+
+public class PromotionsControllerITests : IClassFixture<WebApplicationFactory<Program>>
 {
-    public class PromotionsControllerITests : IClassFixture<WebApplicationFactory<Program>>
+    private readonly HttpClient _client;
+
+    public PromotionsControllerITests(WebApplicationFactory<Program> factory)
     {
-        private readonly HttpClient _client;
+        _client = factory.CreateClient();
+    }
 
-        public PromotionsControllerITests(WebApplicationFactory<Program> factory)
-        {
-            _client = factory.CreateClient();
-        }
+    [Fact]
+    public async Task GivenValidParameters_WhenGettingPromotions_ThenReturnsOk()
+    {
+        // Arrange
+        var url = "/v1/ES/promotions?languageCode=ES";
 
-        [Fact]
-        public async Task GivenValidParameters_WhenGettingPromotions_ThenReturnsOk()
-        {
-            // Arrange
-            var url = "/v1/ES/promotions?languageCode=ES";
+        // Act
+        var response = await _client.GetAsync(url);
 
-            // Act
-            var response = await _client.GetAsync(url);
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
+    [Fact]
+    public async Task GivenMissingLanguageCode_WhenGettingPromotions_ThenReturnsBadRequest()
+    {
+        // Arrange: 
+        var url = "/v1/ES/promotions?languageCode=";
 
-        [Fact]
-        public async Task GivenMissingLanguageCode_WhenGettingPromotions_ThenReturnsBadRequest()
-        {
-            // Arrange: 
-            var url = "/v1/ES/promotions?languageCode=";
+        // Act
+        var response = await _client.GetAsync(url);
 
-            // Act
-            var response = await _client.GetAsync(url);
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        }
+    [Fact]
+    public async Task GivenInvalidCountryCode_WhenGettingPromotions_ThenReturnsBadRequest()
+    {
+        // Arrange: 
+        var url = "/v1/E/promotions?languageCode=ES";
 
-        [Fact]
-        public async Task GivenInvalidCountryCode_WhenGettingPromotions_ThenReturnsBadRequest()
-        {
-            // Arrange: 
-            var url = "/v1/E/promotions?languageCode=ES";
+        // Act
+        var response = await _client.GetAsync(url);
 
-            // Act
-            var response = await _client.GetAsync(url);
-
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        }
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 }
--- test/Application.Tests/GetAll/V1/PromotionsControllerTests.cs
+++ test/Application.Tests/GetAll/V1/PromotionsControllerTests.cs
@@ -1,8 +1,8 @@
-﻿using PromotionEngine.Application.Features.Promotions.GetAll.V1;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PromotionEngine.Application.Features.Promotions.GetAll.V1;
 using PromotionEngine.Application.Shared;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using PromotionEngine.Entities;
 
 namespace PromotionEngine.Application.GetAll.V1;
 
@@ -16,17 +16,97 @@ public class PromotionsControllerTests
     {
         _loggerMock = new Mock<ILogger<PromotionsController>>();
         _handlerMock = new Mock<IHandler<GetAllPromotionsRequest, GetAllPromotionsResponse>>();
-        _controller = new PromotionsController(_handlerMock.Object, _loggerMock.Object);
+
+        //We need to add this in order to make the Request.Path of the ProblemDetails,
+        //if not it will be null and the assertion will fail with null reference exception
+        var context = new DefaultHttpContext();
+
+        _controller = new PromotionsController(_handlerMock.Object, _loggerMock.Object)
+        {
+            ControllerContext = new ControllerContext()
+            {
+                HttpContext = context
+            }
+        };
     }
 
     [Fact]
-    public async Task GivenInvalidCountryCode_WhenGettingDiscounts_ThenThrowsValidationException()
+    public async Task GivenInvalidCountryCode_WhenGettingPromotions_ThenThrowsValidationException()
     {
         //Arrange
+        var promotionResults = new List<PromotionModel>() { CreatePromotionModel() };
+        var countryCode = "ES";
+        var lang = "EN";
+        var request = new GetAllPromotionsRequest(countryCode, lang);
+        var response = new GetAllPromotionsResponse().SetPromotions(promotionResults);
 
+        _handlerMock.Setup(r => r.HandleAsync(request, default))
+            .ReturnsAsync(response);
+        
         //Act
-        var result = await _controller.Get(string.Empty, "ES", default);
+        var result = await _controller.Get(countryCode, lang, default);
 
         //Assert
+        _handlerMock.Verify(r => r.HandleAsync(request, default), Times.Once);
+        Assert.IsType<OkObjectResult>(result);
+        var okResult = result as OkObjectResult;
+        Assert.NotNull(okResult);
+        Assert.IsType<GetAllPromotionsResponse>(okResult.Value);
+        Assert.Equal(response, okResult.Value);
+
     }
+
+    [Fact]
+    public async Task GivenNoPromotionsFound_WhenGettingPromotions_ThenReturnsInternalServerError()
+    {
+        //Arrange
+        var lang = "EN";
+        var countryCode = "ES";
+        var request = new GetAllPromotionsRequest(countryCode, lang);
+        var response = new GetAllPromotionsResponse().SetException(new Exception("No promotions found"));
+        
+        _handlerMock.Setup(r => r.HandleAsync(request, default))
+            .ReturnsAsync(response);
+
+        //Act
+        var result = await _controller.Get(countryCode, lang, default);
+
+        //Assert
+        _handlerMock.Verify(r => r.HandleAsync(request, default), Times.Once);
+        Assert.IsType<ObjectResult>(result);
+        var okResult = result as ObjectResult;
+        Assert.NotNull(okResult);
+        Assert.Equal(500, okResult.StatusCode);
+    }
+
+    private static PromotionModel CreatePromotionModel()
+    {
+        return new PromotionModel
+        {
+            PromotionId = Guid.NewGuid(),
+            EndValidityDate = DateTime.UtcNow.AddDays(7),
+            Texts = new PromotionTextsModel
+            {
+                Title = "Test Title",
+                Description = "Test Description",
+                DiscountTitle = "Discount",
+                DiscountDescription = "Discount Description"
+            },
+            Images = new List<string> { "https://example.com/image1.png" },
+            Discounts = new List<Discount>
+            {
+                new OnlineDiscount
+                {
+                    OriginalPrice = 50,
+                    FinalPrice = 35,
+                    LowestPriceLast30Days = 35,
+                    PriceType = "EUR",
+                    UnitsToBuy = 1,
+                    UnitsToPay = 1,
+                    HasPrice = true
+                }
+            }
+        };
+    }
+
 }
